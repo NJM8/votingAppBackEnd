9971d8dd1ebd069a0831e61129434162fb19d68b
Nate Mallison <healthynate@gmail.com>
2018-05-29 11:49:52 -0400

    fixed error handling on sign in route

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -6,18 +6,20 @@ var jwt = require('jsonwebtoken');
 router
   .route('/signin')
   .post((req, res, next) => {
+    console.log(req.body);
     db.Users.findOne({ username: req.body.username }).then(user => {
       user.comparePassword(req.body.password, (err, isMatch) => {
         if (isMatch) {
           const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY);
-          res.status(200).json({ token: token, userId: user.id, username: user.username })
+          res.status(200).json({ idToken: token, userId: user.id, username: user.username })
         } else {
-          res.status(400).send('Invalid Credentials')
+          console.log('wrong password');
+          res.status(401).send('Invalid Password')
         }
       })
-    }, function (err) {
-      res.status(400).send('Invalid Credentials')
-    })
+    }).catch(error => {
+      res.status(401).send('Invalid Username')
+    });
 });
 
 router
@@ -31,7 +33,7 @@ router
     }
     db.Users.create(req.body).then(user => {
       const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY);
-      res.status(200).json({ token: token, userId: user.id, username: user.username })
+      res.status(200).json({ idToken: token, userId: user.id, username: user.username })
     }).catch(error => {
       return next(error);
     })
