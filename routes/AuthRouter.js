969b1eb088a26e0785fe365ffd98b1345e5094a0
Nate Mallison <healthynate@gmail.com>
2018-05-29 12:16:28 -0400

    added expiration date to token and send to client

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -10,14 +10,15 @@ router
     db.Users.findOne({ username: req.body.username }).then(user => {
       user.comparePassword(req.body.password, (err, isMatch) => {
         if (isMatch) {
-          const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY);
-          res.status(200).json({ idToken: token })
+          const expiresIn = 86400;
+          const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: expiresIn });
+          res.status(200).json({ idToken: token, expiresIn: expiresIn });
         } else {
-          return res.status(401).send('Invalid Password')
+          return res.status(401).send('Invalid Password');
         }
       })
     }).catch(error => {
-      return res.status(401).send('Invalid Username')
+      return res.status(401).send('Invalid Username');
     });
 });
 
@@ -25,8 +26,9 @@ router
   .route('/signup')
   .post((req, res, next) => {
     db.Users.create(req.body).then(user => {
-      const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY);
-      res.status(200).json({ idToken: token })
+      const expiresIn = 86400;
+      const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: expiresIn });
+      res.status(200).json({ idToken: token, expiresIn: expiresIn });
     }).catch(error => {
       if (error.code === 11000) {
         if (error.message.split(' ').includes('email_1')) {
@@ -39,6 +41,5 @@ router
       return next(error);
     })
 });
-
 
 module.exports = router;
