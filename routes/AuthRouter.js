66ba3ec708fb53e939b1fd67e9995600de9c4871
Nate Mallison <healthynate@gmail.com>
2018-05-29 14:42:46 -0400

    dealt with bad token or old token on auto login

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -6,13 +6,11 @@ var jwt = require('jsonwebtoken');
 router
   .route('/signin')
   .post((req, res, next) => {
-    console.log(req.body);
     db.Users.findOne({ username: req.body.username }).then(user => {
       user.comparePassword(req.body.password, (err, isMatch) => {
         if (isMatch) {
-          const expiresIn = 86400;
-          const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: expiresIn });
-          res.status(200).json({ idToken: token, expiresIn: expiresIn });
+          const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: 86400 });
+          res.status(200).json({ idToken: token, expiresIn: 86400 });
         } else {
           return res.status(401).send('Invalid Password');
         }
@@ -26,9 +24,8 @@ router
   .route('/signup')
   .post((req, res, next) => {
     db.Users.create(req.body).then(user => {
-      const expiresIn = 86400;
-      const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: expiresIn });
-      res.status(200).json({ idToken: token, expiresIn: expiresIn });
+      const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: 86400 });
+      res.status(200).json({ idToken: 'junk', expiresIn: 86400 });
     }).catch(error => {
       if (error.code === 11000) {
         if (error.message.split(' ').includes('email_1')) {
@@ -40,6 +37,30 @@ router
       }
       return next(error);
     })
+});
+
+router
+  .route('/verifyUser')
+  .post((req, res, next) => {
+    try {
+      jwt.verify(req.body.idToken, process.env.SECRET_KEY, function(error, decoded) {
+        if (decoded) {
+          db.Users.findOne({ _id: decoded.user_id }).then(user => {
+            const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY, { expiresIn: 86400 });
+            return res.status(200).json({ idToken: token, expiresIn: 86400, username: user.username });
+          }).catch(error => {
+            return next(error);
+          });
+        } else {
+          if (error.name === 'TokenExpiredError') {
+            return res.status(401).send('signin');
+          }
+          return res.status(401).send('Invalid Token')
+        }
+      });
+    } catch (error) {
+      return res.status(401).send('Invalid Token')
+    }
 });
 
 module.exports = router;
