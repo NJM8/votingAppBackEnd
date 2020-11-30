d5451ca7734f6d5134292b25f1e5aca50ae590f0
natemallison <healthynate@gmail.com>
2020-11-30 08:58:57 -0500

    Updating base URL.

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const authRouter = require('./routes/AuthRouter')
-const votingRouter = require('./routes/VotingRouter')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const PORT = process.env.PORT || 8000
-const app = express()
-dotenv.load()
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const authRouter = require("./routes/AuthRouter");
+const votingRouter = require("./routes/VotingRouter");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+const app = express();
+dotenv.load();
 
-app.use(cors({
-  origin: app.get('env') === 'development' ? 'http://localhost:8080' : 'https://natethedev-votingappfrontend.herokuapp.com'
-}))
+app.use(
+  cors({
+    origin:
+      app.get("env") === "development"
+        ? "http://localhost:8080"
+        : "http://votingapp.natethedev.com",
+  })
+);
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(morgan('tiny'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
-app.use('/auth', authRouter)
-app.use('/users', votingRouter)
+app.use("/auth", authRouter);
+app.use("/users", votingRouter);
 
 // catch 404 and send to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  return next(err)
-})
+  const err = new Error("Not Found");
+  err.status = 404;
+  return next(err);
+});
 
 // error handler
 app.use((err, req, res, next) => {
-  let mode = app.get('env')
-  if (mode === 'development') {
-    console.log(err)
+  let mode = app.get("env");
+  if (mode === "development") {
+    console.log(err);
   }
-  return res.status(err.status || 500).send(err)
-})
+  return res.status(err.status || 500).send(err);
+});
 
 app.listen(PORT, () => {
-  console.log(`App is being served on port ${PORT}`)
-})
+  console.log(`App is being served on port ${PORT}`);
+});
